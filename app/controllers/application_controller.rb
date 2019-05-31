a7561f939db4b8743215b37c85ea2b2c7e24dea9
Testu <tetsu99soccer@gmail.com>
2019-05-31 22:46:33 +0900

    all code

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery width: :exception
+  add_flash_types :success, :info, :warning, :danger
+
+  include CommonActions
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+    get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#index"
+  get "pages/help"
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+end
